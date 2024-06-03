c43800b9d4df1a565fb3eb4c0d8a78ddf8785819
SparrowWatt <123118108+SparrowWatt@users.noreply.github.com>
2024-06-03 14:35:02 +0200

    test

---
+++ script.js
@@ -0,0 +1,8 @@
+function showFeatures() {
+    var x = document.getElementById("feature-text");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
