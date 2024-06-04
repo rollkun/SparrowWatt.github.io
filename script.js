1cca75d84c5cca4a044090170b786267eba65c48
SparrowWatt <123118108+SparrowWatt@users.noreply.github.com>
2024-06-04 13:40:40 +0200

    Úprava footeru

--- script.js
+++
@@ -1,8 +0,0 @@
-function showFeatures() {
-    var x = document.getElementById("feature-text");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
